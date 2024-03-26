659995dc1973d9c6b7781e72af237bd635e130af
Emmanuel Obolo <e.obolo@alustudent.com>
2024-03-26 18:55:23 +0000

    added crispynesss to the form

--- easy_healthcare/settings.py
+++ easy_healthcare/settings.py
@@ -39,7 +39,14 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "crispy_forms",
+    "crispy_bootstrap5",
 ]
+
+# Template Packs
+CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
+CRISPY_TEMPLATE_PACK = "bootstrap5"
+
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
