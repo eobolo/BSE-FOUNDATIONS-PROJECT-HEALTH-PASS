066b57d10cdf08e9edca1bd5c6babe8b02941c8e
Emmanuel Obolo <obolo.emmanuel31052000@gmail.com>
2024-04-13 12:36:16 +0200

    new updates

--- easy_healthcare/easy_healthcare/settings.py
+++ easy_healthcare/easy_healthcare/settings.py
@@ -15,7 +15,6 @@ from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-print(BASE_DIR)
 
 
 # Quick-start development settings - unsuitable for production
@@ -25,7 +24,7 @@ print(BASE_DIR)
 SECRET_KEY = "django-insecure-%ux=9ptjlzqw3fsv%+hec$9nbczl5%345d(@xi8+04ka!vc8=="
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["*"]
 
