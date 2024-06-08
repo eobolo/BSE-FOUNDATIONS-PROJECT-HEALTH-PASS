1ff15643b5c1a40d5a42993dc52daca8b5ef6478
Obolo Emmanuel <e.obolo@alustudent.com>
2024-06-08 21:33:23 +0300

    trying the redirect to my page for performanve report

--- easy_healthcare/academic_feedback_sys/views_2.py
+++ easy_healthcare/academic_feedback_sys/views_2.py
@@ -5,7 +5,9 @@ from django.urls import reverse
 from django.utils.http import urlencode
 from django.core.mail import EmailMultiAlternatives
 from django.conf import settings
-from . import views
+import plotly.express as px
+
+
 import datetime
 
 
@@ -34,6 +36,89 @@ def send_html_email(urllink, email, school_name="child's"):
     msg.attach_alternative(html_content, "text/html")
     msg.send()
 
+def get_performance_context_bar_charts(grades, current_year):
+    # create a bunch of lists
+    """
+    all_html to help store the embedded htmls
+    per year based on the number of available grades
+    semester names and average stores the respective names
+    and average of all grades per semester and construct
+    a bar plot from there, finally the yearly average, give
+    the average of all semesters.
+    """
+    all_html_graphs = []
+    semester_averages = []
+    semester_names = []
+    yearly_averages = []
+    yearly_name = []
+    context_variable = {}
+
+    # record the grades per subjects
+    for grade in grades:
+        subjects = ["Mathematics", "English", "Physics", "Chemistry", "Biology", "Geography", "Civics", "Agricultural Science", "Economics", "Commerce", "Government", "Nutrition", "Religious Studies", "Technical Drawing", "Literature in English", "Accounting", "Marketing"]
+        scores = [
+            grade.math_score.score,
+            grade.english_score.score,
+            grade.physics_score.score,
+            grade.chemistry_score.score,
+            grade.biology_score.score,
+            grade.geography_score.score,
+            grade.civics_score.score,
+            grade.agricultural_science_score.score,
+            grade.economics_score.score,
+            grade.commerce_score.score,
+            grade.government_score.score,
+            grade.nutrition_score.score,
+            grade.religious_studies_score.score,
+            grade.technical_drawing_score.score,
+            grade.literature_in_english_score.score,
+            grade.accounting_score.score,
+            grade.marketing_score.score
+        ]
+        fig = px.bar(
+            x=subjects,
+            y=scores,
+            color=f"{grade.semester.semester}",
+            height=600,
+            title="Performance report bar chart for each subject",
+            labels={'x': 'Subjects', 'y': 'Scores'}
+        )
+        chart = fig.to_html()
+        context_variable["all_html_grahs"] = all_html_graphs.append(chart)
+        semester_averages.append(sum(scores)/len(scores))
+
+    # now record the average semester(s) grade
+    for i in range(0, len(semester_averages)):
+        semester_names.append(f"semester-{i+1}")
+    
+    fig = px.bar(
+        x=semester_names,
+        y=semester_averages,
+        color="Average per semester",
+        height=600,
+        title="Performance report bar chart per semester",
+        labels={'x': 'Semester', 'y': 'Semester Averages %'}
+    )
+    chart = fig.to_html()
+    context_variable["all_html_grahs"] = all_html_graphs.append(chart)
+
+    if len(grades.all()) == 3:
+        yearly_averages.append(sum(semester_averages)/len(semester_averages))
+        yearly_name.append(f"{current_year}")
+        fig = px.bar(
+            x=yearly_name,
+            y=yearly_averages,
+            color="Average per current year",
+            height=600,
+            title="Performance report bar chart per year",
+            labels={'x': 'Year', 'y': 'Yearly Average %'}
+        )
+        chart = fig.to_html()
+        context_variable["all_html_grahs"] = all_html_graphs.append(chart)
+
+    # now finally return the context variable with all graphs
+    return context_variable
+
 def ParentReportSuccess(request):
     template_name = "academic_feedback_sys/parent_report_success.html"
     return render(request, template_name)
@@ -48,8 +133,32 @@ def ParentReport(request, password):
         return redirect_login_response
     
     if request.session.get(f"{link_uuid_password}", None):
+        # get selected student  and also get the current
+        # year, then use this to filter student grade
+        # based on current year
+        selected_student = Student.objects.filter(
+            secret_password="{0}".format(password),
+        ).first()
+        current_date = datetime.datetime.now()
+        current_year = current_date.year
+        selected_student_grade  = Grade.objects.filter(
+            student=selected_student,
+            semester_year__year=current_year,
+        ).order_by("semester")
+        # check length of selected student grade
+        if len(selected_student_grade) == 0:
+            context_variable = {}
+            context_variable["no_data"] = "No performance report is found yet for {0} {1} {2}".format(
+                selected_student.first_name,
+                selected_student.middle_name,
+                selected_student.last_name
+            )
+            return render(request, success_template_name, context_variable)
+        # delete uuid password in session before getting context_variable
         del request.session[f"{link_uuid_password}"]
-        return render(request, success_template_name)
+        context_variable = get_performance_context_bar_charts(selected_student_grade, current_year)
+        return render(request, success_template_name, context_variable)
+    # return error template
     return render(request, error_template_name)
 
 def ParentLoginPlatform(request):
